dd0ea2a9a83d3b35c1bfda143b767f4350f5e1bc
shrininivasBakare <161009871+shrininivasBakare@users.noreply.github.com>
2024-02-23 13:41:59 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+// import AppNavigator from './Project/navigation/AppNavigator';
+import HomeSearch from './Project/Screens/HomeSearch';
+import MainTabNavigator from './Project/navigation/MainTabNavigator';
+import MainNavigation from './Project/Routes/MainNavigation';
+import ProfileScreen from './Project/Screens/ProfileScreen';
+
+const App = () => {
+  return (
+
+<MainNavigation/>
+
+);
+};
+
+export default App;
